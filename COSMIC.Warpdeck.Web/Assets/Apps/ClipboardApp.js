842d58e4da50af784b6c442f0afcbc460de2a524
Andrew Munro <12575339+armunro@users.noreply.github.com>
2023-12-27 12:35:31 -0600

    Create vue app for clipboard

---
+++ COSMIC.Warpdeck.Web/Assets/Apps/ClipboardApp.js
@@ -0,0 +1,32 @@
+import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+//import Wd from "../../components/Timer.js"
+
+let api;
+
+let app = createApp({
+    data() {
+        return {
+            api: null,
+            clips: [{text: ''}]
+        }
+    },
+    methods: {
+        loadClips() {
+            api.getClips().then(value => this.clips = value)
+        },
+        triggerAction(actionTypeName, parametersString) {
+            api.triggerAction(actionTypeName, parametersString).then(response => console.log(response) )
+        }
+    },
+    mounted() {
+        api = new WarpdeckApi("http://localhost:4300/api")
+        this.api = api;
+        this.loadClips();
+    },
+});
+
+
+//app.component("fptasklist", FpTaskList);
+app = app.mount("#clipboardApp");
+document.app = app;
+
--- COSMIC.Warpdeck.Web/Assets/Components/ComponentGrid.js
+++ COSMIC.Warpdeck.Web/Assets/Components/ComponentGrid.js
@@ -1,17 +1,32 @@
 export default {
     props: {
-        componentModel: Object
+        componentModel: Object,
+        columns: Number,
+        rows: Number
     },
     data (){
         return {
         }
     },
     methods: {
-       
+        calculateIconUrl() {
+            return "/api/device/" + this.deviceId + "/layer/" + this.layerId + "/icon";
+        },
+        calculateLayerKeysUri() {
+            return "/api/device/" + this.deviceId + "/layer/" + this.layerId + "/key"
+        },
+
+        calculateLayerKeyUri(keyId) {
+            return "/api/device/" + this.deviceId + "/layer/" + this.layerId + "/key/" + keyId
+        }
     },
     mounted() {
     },
     template: `
-
-  `
+        <div v-for="r in rows">
+            <div v-for="c in columns">
+            hi
+            </div>
+        </div>
+    `
 }
